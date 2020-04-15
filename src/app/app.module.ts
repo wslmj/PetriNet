e604813731eea68d98ef3b06e8b995167803c45b
unknown <lin6555@uwlax.edu>
2020-04-15 02:16:58 -0500

    commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetriNetComponent } from './petri-net/petri-net.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 // import { PetriService } from './service/petri.service';
 @NgModule({
   declarations: [
@@ -20,7 +19,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
